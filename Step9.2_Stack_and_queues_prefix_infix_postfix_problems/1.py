0473506202f892e7321b662952a64c9c0d79018c
Ravinder Kumar <ravinderverma872@gmail.com>
2024-07-25 20:47:23 -0500

    Implemented infix to postfix conversion using stack

---
+++ Step9.2_Stack_and_queues_prefix_infix_postfix_problems/1.py
@@ -0,0 +1,33 @@
+#Infix to postfix conversion using stack
+# Not pragmatic to implement all operations(+, -, *) in one scan in infix conversion so we need to convert into postfix
+# or prefix through symbol and their precedence
+
+# equation should make fully parenthesis using parenthesisation(precedence)
+
+def get_symbol_precedence(prec, chr):
+    for key,val in prec.items():
+        if key == chr:
+            return val
+
+def infix_to_postfix(s, stk, prec):
+    i = 0
+    post = ""
+    while i < len(s):
+        if s[i].isalnum():
+            post = post + s[i]
+        else:
+            while stk and get_symbol_precedence(prec, s[i]) <= get_symbol_precedence(prec, stk[-1]):
+                post = post + stk.pop()
+            stk.append(s[i])
+        i += 1
+    while stk:
+        post = post + stk.pop()
+    return post
+
+s = "a+b*c-d/e"
+stk = []
+prec = {'+': 1, '-': 1, '*': 2, '/': 2}
+
+post = infix_to_postfix(s, stk, prec)
+
+print(post)
