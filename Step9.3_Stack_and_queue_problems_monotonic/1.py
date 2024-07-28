b4d166a6811b384bff466b43ca66251b458f5e3c
Ravinder Kumar <ravinderverma872@gmail.com>
2024-07-28 16:07:50 -0500

    Next greater element in normal array

--- Step9.3_Stack_and_queue_problems_monotonic/1.py
+++ Step9.3_Stack_and_queue_problems_monotonic/1.py
@@ -1,17 +1,21 @@
-# Next greater element using stack
+# Next greater element using stack in normal array
 
 def NGE(arr):
-    n = len(arr)
     stack = []
-    result = [-1] * n
-    for i in range (2 * n-1, -1, -1):
-        while stack and stack[-1] <= arr[i%n]:
-            stack.pop()
-        if i < n:
+    result = []
+
+    for item in arr[::-1]:
+        if not stack:
+            result.append(-1)
+        else:
+            while stack and stack[-1] <= item:
+                stack.pop()
             if stack:
-                result[i] = stack[-1]
-        stack.append(arr[i%n])
-    return result
+                result.append(stack[-1])
+            else:
+                result.append(-1)
+        stack.append(item)
+    return result[::-1]
 
 arr = [3,10,4,2,1,2,6,1,7,2,9]
 
