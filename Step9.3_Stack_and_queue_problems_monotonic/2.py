82ba2184a1bf26864ddf83ed6994fd10a89cad2d
Ravinder Kumar <ravinderverma872@gmail.com>
2024-07-28 16:08:17 -0500

    Next greater element in circular array

---
+++ Step9.3_Stack_and_queue_problems_monotonic/2.py
@@ -0,0 +1,18 @@
+#Next greater element in circular array using stack
+
+def NGE(arr):
+    n = len(arr)
+    stack = []
+    result = [-1] * n
+    for i in range (2 * n-1, -1, -1):
+        while stack and stack[-1] <= arr[i%n]:
+            stack.pop()
+        if i < n:
+            if stack:
+                result[i] = stack[-1]
+        stack.append(arr[i%n])
+    return result
+
+arr = [3,10,4,2,1,2,6,1,7,2,9]
+
+print(NGE(arr))
