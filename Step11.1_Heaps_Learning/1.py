a87e774d74f7a537caafcdb557cb218f3c5155d1
Ravinder Kumar <ravinderverma872@gmail.com>
2024-08-17 18:58:30 -0500

    Added new directory

--- Step11.1_Heaps_Learning/1.py
+++ Step11.1_Heaps_Learning/1.py
@@ -22,34 +22,68 @@ arr = 30, 20, 15, 5, 10, 12, 6 40 .. ..
 Insert algo:
     call algo after inserting element at free space
     Takes log(n) time
-Creating heap algo:
+Creating heap algo: O(nlogn)
     It is inplace heap
+    call insert function for each element
+Deleting from heap:O(nlogn)
+    we are using max heap here so only can delete maximum element or can say highest priority element
+    Approach is delete the max element and readjust it to max heap again
+    Compare the children and if greater than interchange with parent
+Heap sort:O(nlogn)
+    i) Create heap of n elements
+    ii) Delete n elements 1 by 1
 """
 
 def insert(arr, N):
     i = N
     temp = arr[N]
-
-    while i > 0 and temp > arr[i//2]:
-        arr[i] = arr[i//2]
-        i = i // 2
+    
+    while i > 0 and temp > arr[(i-1)//2]:
+        arr[i] = arr[(i-1)//2]
+        i = (i-1) // 2
     arr[i] = temp
-    print(arr)
 
-def createHeap():
-    arr = [14, 12, 1 , 19, 13, 28, 45]
+def createHeap(arr):
     for i in range(1, len(arr)):
         insert(arr, i)
 
+def deleteFromHeap(arr, N):
+    i = 0
+    temp = arr[0]
+    arr[0] = arr[N-1]  # Move the last element to the root
+    N -= 1  # Reduce the heap size
+    
+    while True:
+        left = 2*i + 1
+        right = 2*i + 2
+        largest = i
+        
+        if left < N and arr[left] > arr[largest]:
+            largest = left
+        if right < N and arr[right] > arr[largest]:
+            largest = right
+        
+        if largest == i:
+            break
+        
+        arr[i], arr[largest] = arr[largest], arr[i]
+        i = largest
+
+    arr[N] = temp
+
+def heapSort():
+    arr = [10,20,30,25,5,40,35]
+    createHeap(arr)
+    print("Max heap array: ", arr)
+
+    for i in range(len(arr)-1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]  # Swap root with the last element
+        deleteFromHeap(arr, i)  # Re-heapify the reduced heap
+    
+    print("Sorted array: ", arr)
 
 def main():
-    arr = [30, 20, 15, 5, 10, 12, 6]
-    element = 40
-    arr.append(element)
-    N = len(arr) - 1
-    insert(arr, N)
-    createHeap()
-
+    heapSort()
 
 
 if __name__ == "__main__":
