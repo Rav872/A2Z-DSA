b55d482d7a7362269f040b9583312fe430c34680
Ravinder Kumar <ravinderverma872@gmail.com>
2024-09-10 17:45:25 -0500

    Insert in binary search tree

---
+++ Step14.2_BST_Problems/3.py
@@ -0,0 +1,58 @@
+
+"""
+Description: Insert in a binary search tree
+
+"""
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def printBST(self, root):
+        if root is None:
+            return
+        print(root.data, end=": ")
+        if root.left:
+            print("L:", root.left.data, end=" ")
+        if root.right:
+            print("R:", root.right.data, end=" ")
+        print()
+        self.printBST(root.left)
+        self.printBST(root.right)
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self.root = self._insert(self.root, value)
+        return self.root
+
+    def _insert(self, root, value):
+        if value < root.data:
+            if root.left:
+                root.left = self._insert(root.left, value)
+            else:
+                root.left = Node(value)
+        else:
+            if root.right:
+                root.right = self._insert(root.right, value)
+            else:
+                root.right = Node(value)
+        return root
+
+
+def main():
+    bst = BST()
+    arr = [8, 12, 7, 2, 78, 11]
+    for num in arr:
+        root = bst.insert(num)
+    bst.printBST(root)
+
+if __name__ == '__main__':
+    main()
